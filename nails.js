31ac5af987b80fa276c7e8b02f7970fd11e8c876
Samuel <ekemasamuel954@gmail.com>
2024-04-03 16:18:51 -0400

    edited-shop-section and added customer review section

--- nails.js
+++ nails.js
@@ -33,6 +33,24 @@ menuBar.addEventListener("click", () =>{
 })
 
 
+//review slide
+
+let currentReviewIndex = 1;
+const totalReviews = 3;
+
+function showNextReview() {
+    document.querySelector(`.review${currentReviewIndex}`).style.display = 'none';
+    currentReviewIndex = (currentReviewIndex % totalReviews) + 1;
+    document.querySelector(`.review${currentReviewIndex}`).style.display = 'block';
+}
+
+function showPrevReview() {
+    document.querySelector(`.review${currentReviewIndex}`).style.display = 'none';
+    currentReviewIndex = (currentReviewIndex - 2 + totalReviews) % totalReviews + 1;
+    document.querySelector(`.review${currentReviewIndex}`).style.display = 'block';
+}
+
+
 
 
 
