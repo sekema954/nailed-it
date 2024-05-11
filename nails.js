45c36acdc1f6899a95a7ca1aa6bf26318a20dce5
sekema954 <sekema954@gmail.com>
2024-05-11 01:25:50 -0400

    initial commit

--- nails.js
+++ nails.js
@@ -26,50 +26,60 @@ window.addEventListener("scroll", ()=>{
 
 
 //slider-carousel
-//select slides
-const slides = document.querySelectorAll(".slider");
-const slide1 = document.querySelector(".slide1");
-const slide2 = document.querySelector(".slide2");
-const slide3 = document.querySelector(".slide3");
-
-//selecct radio btns
-const radio1 = document.querySelector('.radio1');
-const radio2 = document.querySelector('.radio2');
-const radio3 = document.querySelector('.radio3');
-
-
-
-function resetSlides() {
-    slide1.classList.remove('active-slide');
-    slide2.classList.remove('active-slide');
-    slide3.classList.remove('active-slide');
-}
-
-function resetColor() {
-    radio1.classList.remove('active-btn');
-    radio2.classList.remove('active-btn');
-    radio3.classList.remove('active-btn');
-}
-
-radio1.addEventListener("click", () => {
-    resetColor();
-    resetSlides();
-    radio1.classList.add('active-btn');
-    slide1.classList.add('active-slide');
-});
-
-radio2.addEventListener("click", () => {
-    resetColor();
-    resetSlides();
-    radio2.classList.add('active-btn');
-    slide2.classList.add('active-slide');
-});
-
-radio3.addEventListener("click", () => {
-    resetColor();
-    resetSlides();
-    radio3.classList.add('active-btn');
-    slide3.classList.add('active-slide');
+// set slides to display none.
+const carouselSliders = document.querySelectorAll('.carouselSlider');
+carouselSliders.forEach(slide =>{
+    slide.style.display = 'none';
 });
 
 
+//select slides,
+const slider1 = document.getElementById('slider1');
+const slider2 = document.getElementById('slider2');
+const slider3 = document.getElementById('slider3');
+
+
+//get radio btns
+const radio1 = document.querySelector(".radio1");
+const radio2 = document.querySelector(".radio2");
+const radio3 = document.querySelector(".radio3");
+
+function resetSlider(){
+    slider1.classList.remove("active-slider");
+    slider2.classList.remove("active-slider");
+    slider3.classList.remove("active-slider");
+
+};
+
+function resetRadio(){
+    radio1.classList.remove('active-radio');
+    radio2.classList.remove('active-radio');
+    radio3.classList.remove('active-radio');
+};
+
+
+
+radio1.addEventListener("click", ()=>{
+    resetRadio();
+    resetSlider();
+    radio1.classList.add('active-radio');
+    slider1.classList.add("active-slider");
+} );
+
+radio2.addEventListener("click", ()=>{
+    resetRadio();
+    resetSlider();
+    radio2.classList.add('active-radio');
+    slider2.classList.add("active-slider");
+} );
+
+
+radio3.addEventListener("click", ()=>{
+    resetRadio();
+    resetSlider();
+    radio3.classList.add('active-radio');
+    slider3.classList.add("active-slider");
+} );
+
+
+//set slides to transition
