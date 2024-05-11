78453fc037cca4e57cc7514f152026f93f82eeb1
sekema954 <sekema954@gmail.com>
2024-05-11 17:47:46 -0400

    initial commit

--- nails.js
+++ nails.js
@@ -82,4 +82,91 @@ radio3.addEventListener("click", ()=>{
 } );
 
 
-//set slides to transition
+
+//service carousel
+
+//select service-boxes
+const serviceImg1 = document.querySelector(".service-box1");
+const serviceImg2 = document.querySelector(".service-box2");
+const serviceImg3 = document.querySelector(".service-box3");
+const serviceImg4 = document.querySelector(".service-box4");
+
+
+//select-radio btns
+
+const radioBtn1 = document.querySelector(".radio-1");
+const radioBtn2 = document.querySelector(".radio-2");
+const radioBtn3 = document.querySelector(".radio-3");
+const radioBtn4 = document.querySelector(".radio-4");
+
+
+//select service prices
+
+const wrapper1 = document.querySelector(".wrapper1");
+const wrapper2 = document.querySelector(".wrapper2");
+const wrapper3 = document.querySelector(".wrapper3");
+const wrapper4 = document.querySelector(".wrapper4");
+
+
+//reset serviceboxes
+
+function resetBoxes(){
+    serviceImg1.classList.remove("active-image");
+    serviceImg2.classList.remove("active-image");
+    serviceImg3.classList.remove("active-image");
+    serviceImg4.classList.remove("active-image");
+};
+
+
+function resetRadioBtns(){
+    radioBtn1.classList.remove('active-radio');
+    radioBtn2.classList.remove('active-radio');
+    radioBtn3.classList.remove('active-radio');
+    radioBtn4.classList.remove('active-radio');
+};
+
+function resetWrapper(){
+    wrapper1.classList.remove("active-container");
+    wrapper2.classList.remove("active-container");
+    wrapper3.classList.remove("active-container");
+    wrapper4.classList.remove("active-container");
+}
+
+//add click events
+radioBtn1.addEventListener("click", ()=>{
+    resetBoxes();
+    resetRadioBtns();
+    resetWrapper();
+    serviceImg1.classList.add("active-image");
+    radioBtn1.classList.add('active-radio');
+    wrapper1.classList.add("active-container");
+});
+
+radioBtn2.addEventListener("click", ()=>{
+    resetBoxes();
+    resetRadioBtns();
+    resetWrapper();
+    serviceImg2.classList.add("active-image");
+    radioBtn2.classList.add('active-radio');
+    wrapper2.classList.add("active-container");
+});
+
+radioBtn3.addEventListener("click", ()=>{
+    resetBoxes();
+    resetRadioBtns();
+    resetWrapper();
+    serviceImg3.classList.add("active-image");
+    radioBtn3.classList.add('active-radio');
+    wrapper3.classList.add("active-container");
+});
+
+
+
+radioBtn4.addEventListener("click", ()=>{
+    resetBoxes();
+    resetRadioBtns();
+    resetWrapper();
+    serviceImg4.classList.add("active-image");
+    radioBtn4.classList.add('active-radio');
+    wrapper4.classList.add("active-container");
+});
